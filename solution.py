325ea26447991ac78dda74b500dc110cb71e6738
asitek17 <122224673+asitek17@users.noreply.github.com>
2023-04-14 02:28:59 +0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd 
 import numpy as np 
-from scipy.stats import cramervonmises_2samp 
+from scipy.stats import ks_2samp
+ 
  
 chat_id = 694905952 # Ваш chat ID, не меняйте название переменной
  
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия 
     alpha = 0.03 
-    pvalue = cramervonmises_2samp(x, y).pvalue 
+    pvalue = ks_2samp(x, y, alternative='less').pvalue 
      
     return pvalue < alpha
