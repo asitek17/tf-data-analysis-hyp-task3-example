45a724db6ea947dca20c7d4061ca6c5d7f65d1f7
asitek17 <122224673+asitek17@users.noreply.github.com>
2023-04-14 01:55:30 +0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,11 @@
 import pandas as pd 
 import numpy as np 
-from scipy.stats import mannwhitneyu 
- 
+from scipy.stats import wilcoxon 
  
 chat_id = 694905952 # Ваш chat ID, не меняйте название переменной
  
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия 
     alpha = 0.03 
-    pvalue = mannwhitneyu(x, y, alternative="less").pvalue 
+    pvalue = wilcoxon(x, y, alternative="less", method='exact', zero_method = 'zsplit').pvalue 
      
     return pvalue < alpha
