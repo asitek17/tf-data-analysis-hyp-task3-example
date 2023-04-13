f1eb2da48b94f2f91a5f9f3a04aede4cc7b121ec
asitek17 <122224673+asitek17@users.noreply.github.com>
2023-04-14 01:21:33 +0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
-import pandas as pd
-import numpy as np
-
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+import pandas as pd 
+import numpy as np 
+from scipy.stats import mannwhitneyu 
+ 
+ 
+chat_id = 694905952 # Ваш chat ID, не меняйте название переменной
+ 
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия 
+    alpha = 0.03 
+    pvalue = mannwhitneyu(x, y, alternative="less").pvalue 
+     
+    return pvalue < alpha
