3f89aada45fa0b3cafdef8e7bea5a77032fa7fad
asitek17 <122224673+asitek17@users.noreply.github.com>
2023-04-14 02:39:42 +0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,11 @@
 import pandas as pd 
 import numpy as np 
-from scipy.stats import ks_2samp
- 
+from scipy.stats import wilcoxon 
  
 chat_id = 694905952 # Ваш chat ID, не меняйте название переменной
  
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия 
     alpha = 0.03 
-    pvalue = ks_2samp(x, y, alternative='less').pvalue 
+    pvalue = wilcoxon(x, y, alternative="less").pvalue 
      
     return pvalue < alpha
