bffe43b8c26a7e15cb610c7eabc8f76de6108a17
asitek17 <122224673+asitek17@users.noreply.github.com>
2023-04-14 02:20:09 +0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd 
 import numpy as np 
-from scipy.stats import wilcoxon 
+from scipy.stats import cramervonmises_2samp 
  
 chat_id = 694905952 # Ваш chat ID, не меняйте название переменной
  
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия 
     alpha = 0.03 
-    pvalue = wilcoxon(x, y, alternative="less", method='exact', zero_method = 'zsplit').pvalue 
+    pvalue = cramervonmises_2samp(x, y).pvalue 
      
     return pvalue < alpha
